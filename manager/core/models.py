654202bb269584677c0b1d957ccfd8d22157f268
Meet Mehta <meet.mehta@think360.ai>
2025-03-19 16:53:46 +0530

    started manager backend

---
+++ manager/core/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True,blank=False,null=False)
+    
+    def __str__(self):
+        return f"User id {self.id}, email {self.email}"
+    
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=100,blank=False,null=False)
+    item_required = models.BooleanField(default=False)
+    item_cost = models.IntegerField(blank=False,null=False)
+    item_stock = models.IntegerField(blank=False,null=False)
+    min_quantity = models.IntegerField(blank=False,null=False)
+    
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="items")
+    
+    def __str__(self):
+        return f"Item id {self.id}, name {self.item_name}"
+    
+class Transaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=100,blank=False,null=False)
+    item_cost = models.IntegerField(blank=False,null=False)
+    transaction_date_time = models.CharField(max_length=100,blank=False,null=False)
+    transaction_type = models.CharField(max_length=10,blank=False,null=False)
+    transaction_units = models.IntegerField(blank=False,null=False)
+    final_stock = models.IntegerField(blank=False,null=False)
+    
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="transactions")
+
+    
+    def __str__(self):
+        return f"Transaction id {self.id}, name {self.item_name}"
---
+++ manager/transactions/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from core.models import Transaction
+
+class TransactionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Transaction
+        fields = '__all__'
